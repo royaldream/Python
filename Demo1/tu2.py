4924367fc7fcd9ccd9d8924d0c36d81e1e45dc2b
royaldream <royparth2094@gmail.com>
2019-03-15 00:04:05 +0530

    abc

---
+++ Demo1/tu2.py
@@ -0,0 +1,13 @@
+snack = ["Pizza", "Dosa", "Burger", "Roti", 100]
+print snack.reverse()
+print snack
+print snack[2]
+numbers = [100, 90, 300, 150, 500]
+print numbers[1:3]
+# print numbers.sort()
+print numbers
+numbers.append(200)
+numbers.insert(2, 30)
+print numbers
+print min(numbers[1:4])
+print max(numbers[1:4])
