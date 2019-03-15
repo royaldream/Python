6aff4c2261264d890987d7722db2c8164301807c
royaldream <royparth94@gmail.com>
2019-03-15 18:39:38 +0530

    tu5-6-7

---
+++ tu5.py
@@ -0,0 +1,14 @@
+n1 = int(input())
+n2 = int(input())
+
+if n1 > n2:
+    print (n1)
+else:
+    print (n2)
+    n1=int(10)
+
+if n1 == n2:
+    print "Equals"
+elif n1 >= n2:
+    print "Greterthan"
+# for(b=0; b<10; b++)
---
+++ tu6.py
@@ -0,0 +1,9 @@
+foods = ["Dosa", "Pizza", "Munchurian", "Soup"]
+for food in foods:
+    print food
+for ch in foods[3]:
+    print ch
+i = 0
+while i < len(foods):
+    print foods[i]
+    i+=1
